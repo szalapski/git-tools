1e58fa6239340edfebdc2faec2eb09b44274d533
Patrick Szalapski <patrick.szalapski@genmills.com>
2026-06-11 12:06:40 -0500

    Revise script requirements for external tools

--- gmm.ps1
+++ gmm.ps1
@@ -1,7 +1,6 @@
 # gmm.ps1: Git Merge from main - 
 # Merges the latest changes from remote master/main to the current local branch
-# Note, this script REQUIRES that you have tee, grep, sed, and paste in your PATH.
-# Get then from UnxUtils (they are in the wbin folder)
+# Note, this script REQUIRES that you have grep, sed, and paste (from UnxUtils), which are included in the repo.
 
 $main = $(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
 git fetch && echo ======== && git merge origin/$main | Tee-Object \\.\CON | grep "CONFLICT" | sed -r "s/^.*conflict in //" | paste -sd " " - | sed -e "s/^./You may want to run: code.cmd -n \0/"
